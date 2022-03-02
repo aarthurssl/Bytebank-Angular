4b1d7942cd0e5a13a4230ea38f0d6baa21d840a6
Arthur Silva <arthurss2156@gmail.com>
2022-03-02 00:48:38 -0300

    Novo Componente Extrato e listando transferencias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'bytebank';
 
-  transferencia: any;
+  transferencias: any[] = [];
 
   transferir($event: any){
     console.log($event);
-    this.transferencia = $event;
+    //Desconstroi o objeto evendo passando as propriedades(Valor e destino)
+    //atraves do "spread operator"
+    const transferencia = {...$event, data: new Date() };
+    this.transferencias?.push(transferencia);
   }
 
 }
